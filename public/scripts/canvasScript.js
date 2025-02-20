a3d480de60c07aa48e3920efb0730f29b1b29398
Madeleine Chance <mechance782@gmail.com>
2025-02-20 14:39:39 -0800

    changing organization

--- public/scripts/canvasScript.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById("canvasContainer");
-
-canvas.addEventListener("click", (event) => {
-    let element = event.target;
-    if (element.id.includes("pixel") ){
-        element.style.backgroundColor = "black";
-    }
-})
-
